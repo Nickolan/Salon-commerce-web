0c6f369e637fc374a4d5a562d1b6b792385701c9
Rafaaxx <rafa.navarro.2006@gmail.com>
2025-09-15 19:27:42 -0300

    cambios en la utn

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import { Route, Routes } from "react-router-dom";
 import HomeScreen from './screens/HomeScreen';
 import Navbar from './Components/Navbar';
-<title>Focus Room</title>
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div style={{ paddingTop: "80px", "--primary-color": "#6A0DAD", "--second-color": "#7B2FF7", "--background-color": "#f3f3fe" }}>
-       <Navbar user={null} />
+       
       <Routes>
-        <Route path="/" element={<> <HomeScreen/> </>} />
+        <Route path="/" element={<> <Navbar user={null} /> <HomeScreen/> </>} />
       </Routes>
     </div>
   )
