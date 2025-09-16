2dfb146a59021142107c78a6ee3084cff8914271
Lucas <lucas.gordillo.3591@gmail.com>
2025-09-16 20:48:51 -0300

    Aún esta incompleto

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Sidebar.css";
+import CerrarSesionLogo from "../../assets/img/CerrarSesion-logo.png";
+import FavoritoLogo from "../../assets/img/Favorito-logo.png";
+import ReservasLogo from "../../assets/img/Reservas-logo.png";
+import SalonesLogo from "../../assets/img/Salones-logo.png";
+import VentasLogo from "../../assets/img/Ventas-logo.png";
+
+const Sidebar = () => {
+    return (
+        <sidebar className="sidebar">
+            <a href="#">Mi Perfil</a>
+            <div className="sidebar-container">
+                <div className="side-links">
+                    <div className="link-item">
+                        <img src={ReservasLogo} alt="Mis Reservas" className="logo" />
+                        <a href="/">Mis Reservas</a>
+                    </div>
+                    <div className="link-item">
+                        <img src={FavoritoLogo} alt="Favoritos" className="logo" />
+                        <a href="/">Favoritos</a>
+                    </div>
+                    <div className="link-item">
+                        <img src={SalonesLogo} alt="Mis Salones" className="logo" />
+                        <a href="/">Mis Salones</a>
+                    </div>
+                    <div className="link-item">
+                        <img src={VentasLogo} alt="Mis Ventas" className="logo" />
+                        <a href="/">Mis Ventas</a>
+                    </div>
+                    <div className="link-item">
+                        <img src={CerrarSesionLogo} alt="Cerrar Sesión" className="logo" />
+                        <a href="/">Cerrar Sesión</a>
+                    </div>
+                </div>
+            </div>
+        </sidebar>
+    );
+};
+
+export default Sidebar;
