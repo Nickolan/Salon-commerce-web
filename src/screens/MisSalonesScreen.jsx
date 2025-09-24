724fea72e38438f103e47b0f0dead1d0e0dbf166
Lautaro Joel Ferreria <lautiferreria@gmail.com>
2025-09-24 08:33:37 -0300

    .

--- src/screens/MisSalonesScreen.jsx
+++ src/screens/MisSalonesScreen.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import BarraBusqueda from '../Components/BarraBusqueda/BarraBusqueda';
 import "../styles/MisSalonesScreen.css";
